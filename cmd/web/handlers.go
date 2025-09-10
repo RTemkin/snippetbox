139a756736ef0cce581b045a5ef6c2e571f5cfe1
RTemkin <temkin17@yandex.ru>
2025-09-10 20:18:08 +0400

    struct2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -15,7 +15,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Привет из Spippetbox"))
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -25,10 +25,10 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Отображение определенной заметки с ID %d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func CreateSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод не дозволен", 405)
+		http.Error(w, "Метод не дозволен", http.StatusMethodNotAllowed)
 		return
 	}
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/create", CreateSnippet)
 
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
